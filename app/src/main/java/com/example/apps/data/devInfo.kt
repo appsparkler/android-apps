24ce865a280d11ed120e6fff1eef95aac6d93a9c
Aakash Shah <subs@appsparkler.com>
2023-04-23 14:45:40 +0530

    feat: setup data

---
+++ app/src/main/java/com/example/apps/data/devInfo.kt
@@ -0,0 +1,19 @@
+package com.example.apps.data
+
+data class DeveloperInfo(
+    val name: String,
+    val punchline: String,
+    val email: String,
+    val mobileNumber: String,
+    val socialMediaHandle: String
+)
+
+object devInfo {
+    val data = DeveloperInfo(
+        name = "Aakash Shah",
+        email = "appsparkler@gmail.com",
+        mobileNumber = "+91 733 8080 855",
+        punchline = "Android Developer Extraordinaire",
+        socialMediaHandle = "@appsparkler"
+    )
+}
