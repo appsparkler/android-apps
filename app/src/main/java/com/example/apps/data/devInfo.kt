41856fd84e639608ce867182c406353fef4730b9
Aakash Shah <subs@appsparkler.com>
2023-04-23 15:30:29 +0530

    feat: setup AndroidWithLogoAndLabel component

--- app/src/main/java/com/example/apps/data/devInfo.kt
+++ app/src/main/java/com/example/apps/data/devInfo.kt
@@ -2,6 +2,7 @@ package com.example.apps.data
 
 data class DeveloperInfo(
     val name: String,
+    val label: String,
     val punchline: String,
     val email: String,
     val mobileNumber: String,
@@ -11,6 +12,7 @@ data class DeveloperInfo(
 object devInfo {
     val data = DeveloperInfo(
         name = "Aakash Shah",
+        label = "android",
         email = "appsparkler@gmail.com",
         mobileNumber = "+91 733 8080 855",
         punchline = "Android Developer Extraordinaire",
